84fa159e6105cce6b74a1d7c005f6c68f931a68c
Rave1s <arturas.timofejevas05@gmail.com>
2025-09-14 23:04:53 +0300

    Uzpildytas header failas pagrindinemis dalimis

---
+++ RadixTree.h
@@ -0,0 +1,53 @@
+#ifndef RADIX_H
+#define RADIX_H
+
+#include <iostream>
+#include <string>
+#include <memory>
+
+//TODO add explanatory comments
+namespace RadixTreeProject {
+    class RadixTree {
+        private:
+            class RadixImpl;
+            std::unique_ptr<RadixImpl> pImpl;
+
+        public:
+            using ValueType = std::string;
+
+            RadixTree();
+
+            RadixTree(const RadixTree& other);
+
+            RadixTree& operator=(const RadixTree& other);
+
+            RadixTree(RadixTree&& other) noexcept;
+
+            RadixTree& operator=(RadixTree&& other) noexcept;
+
+            void insert(const ValueType& word);
+
+            bool search(const ValueType& word);
+
+            void remove(const ValueType& word);
+
+            std::string toString() const;
+
+            bool operator==(const RadixTree& other) const;
+            bool operator!=(const RadixTree& other) const;
+            bool operator<(const RadixTree& other) const;
+            bool operator>(const RadixTree& other) const;
+            bool operator<=(const RadixTree& other) const;
+            bool operator>=(const RadixTree& other) const;
+
+            RadixTree& operator+=(const ValueType& word);
+            RadixTree& operator-=(const ValueType& word);
+            void operator!();
+            bool operator[](const ValueType& word) const;
+
+    };
+
+    //TODO exception class
+}
+
+#endif
