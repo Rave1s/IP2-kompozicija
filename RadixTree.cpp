b9da826a79c93a3331879aa7a1dfcd09a2c4140f
Rave1s <arturas.timofejevas05@gmail.com>
2025-09-15 02:31:00 +0300

    Implementuotas insert() metodas

--- RadixTree.cpp
+++ RadixTree.cpp
@@ -9,8 +9,8 @@ namespace RadixTreeProject {
             struct RadixNode {
                 ValueType word;
                 std::unordered_map<char, std::unique_ptr<RadixNode>> children;
-                bool is_leaf;
-                RadixNode(const ValueType& wrd = "") : word(word), is_leaf(false) {
+                bool isLeaf;
+                RadixNode(const ValueType& wrd = "") : word(word), isLeaf(false) {
 
                 };
             };
@@ -25,14 +25,61 @@ namespace RadixTreeProject {
                 freeMemory();
             }
 
-            
+            void insert(const ValueType& word) {
+                RadixNode* node = root.get();
+                size_t index = 0;
+
+                while (index < word.size()) {
+                    char c = word[index];
+                    if (node->children.count(c) == 0) {
+                        node->children[c] = std::make_unique<RadixNode>(word.substr(index));
+                        node->children[c]->isLeaf = true;
+                        return;
+                    }
+
+                    RadixNode* child = node->children[c].get();
+                    size_t matchingLength = 0;
+                    while(matchingLength < child->word.size() && index + matchingLength < word.size() && word[index + matchingLength] == child->word[matchingLength]) {
+                        ++matchingLength;
+                    }
+                    if (matchingLength == child->word.size()) {
+                        node = child;
+                        index += matchingLength;
+                    }
+                    else {
+                        auto nodeSplit = std::make_unique<RadixNode>(child->word.substr(0, matchingLength));
+                        nodeSplit->children[child->word[matchingLength]] = std::move(node->children[c]);
+                        nodeSplit->children[child->word[matchingLength]]->word = child->word.substr(matchingLength);
+                        nodeSplit->isLeaf = false;
+                        node->children[c] = std::move(nodeSplit);
+
+                        if (index + matchingLength < word.size()) {
+                            node->children[c]->children[word[index + matchingLength]] = std::make_unique<RadixNode>(word.substr(index + matchingLength));
+                            node->children[c]->children[word[index + matchingLength]]->isLeaf = true;
+                        }
+                        else {
+                            node->children[c]->isLeaf = true;
+                        }
+                        return;
+                    }
+                }
+                node->isLeaf = true;
+            }
+
+            bool search(const ValueType& word) {
+
+            }
+
+            void remove(const ValueType& word) {
+
+            }
 
             static std::unique_ptr<RadixNode> copyRadixTree(const RadixNode* node) {
                 if (!node) {
                     return nullptr;
                 }
                 auto copy = std::make_unique<RadixNode>(node->word);
-                copy->is_leaf = node->is_leaf;
+                copy->isLeaf = node->isLeaf;
                 for(const auto& ptr : node->children) {
                     copy->children[ptr.first] = copyRadixTree(ptr.second.get());
                 }
