b4b7d09a4b008e8383bfe7aa6194736328991ee4
Rave1s <arturas.timofejevas05@gmail.com>
2025-09-15 21:35:48 +0300

    Perkeltos CRUD funkcijos, toString(), pataisyta

--- RadixTree.cpp
+++ RadixTree.cpp
@@ -3,10 +3,11 @@
 #include <string>
 #include <unordered_map>
 #include <sstream>
+#include <vector>
 
 namespace RadixTreeProject {
     class RadixTree::RadixImpl{
-        private:
+        public:
             /**
              * @brief Represents a radix tree's node.
              */
@@ -26,7 +27,6 @@ namespace RadixTreeProject {
                 };
             };
 
-        public:
             /**
              * @brief Constructor that automatically craetes a root node.
              */
@@ -40,164 +40,6 @@ namespace RadixTreeProject {
              */
             ~RadixImpl() {
                 deleteTree();
-            }
-
-            void insert(const ValueType& word) {
-                RadixNode* node = root.get();
-                size_t index = 0;
-
-                while (index < word.size()) {
-                    char c = word[index];
-                    /**
-                     * Create a child node if none exists matching the prefix.
-                     * Place the prefix into the child node.
-                    */
-                    if (node->children.count(c) == 0) {
-                        node->children[c] = std::make_unique<RadixNode>(word.substr(index));
-                        node->children[c]->isEndOfWord = true;
-                        return;
-                    }
-
-                    RadixNode* child = node->children[c].get();
-                    size_t matchingLength = 0;
-                    // Check to see how much of the prefix matches.
-                    while (matchingLength < child->word.size() && index + matchingLength < word.size() && word[index + matchingLength] == child->word[matchingLength]) {
-                        ++matchingLength;
-                    }
-                    // If the entire child's prefix matches, move to it.
-                    if (matchingLength == child->word.size()) {
-                        node = child;
-                        index += matchingLength;
-                    }
-                    /**
-                     * Else, split the node, look for how long the two match,
-                     * once they don't match, save the matching part as the new prefix for the child,
-                     * place the rest of child's prefix into a grandchild node,
-                     * and create another grandchild, placing the prefix there.
-                     */
-                    else {
-                        auto nodeSplit = std::make_unique<RadixNode>(child->word.substr(0, matchingLength));
-                        nodeSplit->children[child->word[matchingLength]] = std::move(node->children[c]);
-                        nodeSplit->children[child->word[matchingLength]]->word = child->word.substr(matchingLength);
-                        nodeSplit->isEndOfWord = false;
-                        node->children[c] = std::move(nodeSplit);
-
-                        if (index + matchingLength < word.size()) {
-                            node->children[c]->children[word[index + matchingLength]] = std::make_unique<RadixNode>(word.substr(index + matchingLength));
-                            node->children[c]->children[word[index + matchingLength]]->isEndOfWord = true;
-                        }
-                        else {
-                            node->children[c]->isEndOfWord = true;
-                        }
-                        return;
-                    }
-                }
-                
-                // If the word already exists, throw an exception.
-                if (node->isEndOfWord == true) {
-                    throw MyException("The word already exists in the tree");
-                }
-                // Else mark the current node as the end of the word.
-                else {
-                    node->isEndOfWord = true;
-                }
-            }
-
-            bool search(const ValueType& word) {
-                const RadixNode* node = root.get();
-                size_t index = 0;
-
-                while (index < word.size()) {
-                    char c = word[index];
-
-                    // If the current node doesn't have matching children, return false.
-                    if (node->children.count(c) == 0)
-                    {
-                        return false;
-                    }
-
-                    const RadixNode* child = node->children.at(c).get();
-                    size_t matchingLength = 0;
-
-                    // Check for how long the prefixes match.
-                    while (matchingLength < child->word.size() && index + matchingLength < word.size() && word[index + matchingLength] == child->word[matchingLength]) {
-                        ++matchingLength;
-                    }
-                    // If the length doesn't match (meaning the word is shorter than the child prefix), return false.
-                    if (matchingLength != child->word.size()) {
-                        return false;
-                    }
-
-                    node = child;
-                    index += matchingLength;
-                }
-
-                return node->isEndOfWord;
-            }
-
-            void remove(const ValueType& word) {
-                RadixNode* node = root.get();
-                std::vector<std::pair<RadixNode*, char>> removablePart;
-                size_t index = 0;
-
-                while (node && index < word.size()) {
-                    char c = word[index];
-                    // If the child with matching prefix doesn't exist, throw exception.
-                    auto existingChild = node->children.find(c);
-                    if (existingChild == node->children.end()) {
-                        throw MyException("Word not found. Couldn't remove");
-                    }
-
-                    RadixNode* child = existingChild->second.get();
-                    size_t matchingLength = 0;
-
-                    // Check for how long the prefixes match.
-                    while (matchingLength < child->word.size() && index + matchingLength < word.size() && word[index + matchingLength] == child->word[matchingLength]) {
-                        ++matchingLength;
-                    }
-                    // If the length doesn't match (meaning the word is shorter than the child prefix), throw exception.
-                    if (matchingLength != child->word.size()) {
-                        throw MyException("Word not found. Couldn't remove");
-                    }
-
-                    // Save how much of the tree will be deleted.
-                    removablePart.emplace_back(node, c);
-                    node = child;
-                    index += matchingLength;
-                }
-
-                /**
-                 * If:
-                 * 1) node is null,
-                 * 2) current node's prefix isn't marked as an end of a word OR,
-                 * 3) index doesn't match the length of the word,
-                 * throw exception.
-                 */
-                if (!node || !node->isEndOfWord || index != word.size()) {
-                    throw MyException("Word not found. Couldn't remove");
-                }
-
-                // Remove this prefix as the end of the word.
-                node->isEndOfWord = false;
-
-                /**
-                 * Iterate backwards through the nodes (leaf to root),
-                 * checking to see if the child isn't marked as a word ending,
-                 * and doesn't have children.
-                 * If not - deletes it, else - breaks the cycle.
-                 */
-                for (int i = removablePart.size() - 1; i >= 0; --i) {
-                    RadixNode* parent = removablePart[i].first;
-                    char c = removablePart[i].second;
-                    RadixNode* child = parent->children[c].get();
-
-                    if (!child->isEndOfWord && child->children.empty()) {
-                        parent->children.erase(c);
-                    }
-                    else {
-                        break;
-                    }
-                }
             }
 
             /**
@@ -216,20 +58,6 @@ namespace RadixTreeProject {
                 for (const auto& [character, child] : currentNode->children) {
                     collectAllWords(child.get(), prefix, allWords);
                 }
-            }
-
-
-            std::string toString() const {
-                std::vector<ValueType> allSavedWords;
-                collectAllWords(root.get(), "", allSavedWords);
-                std::ostringstream os;
-
-                for (const ValueType& currentWord : allSavedWords) {
-                    os << currentWord << ", ";
-
-                }
-
-                return os.str();
             }
 
             /**
@@ -308,6 +136,176 @@ namespace RadixTreeProject {
         return *this;
     }
 
+    void RadixTree::insert(const ValueType& word) {
+        RadixImpl::RadixNode* node = pImpl->root.get();
+        size_t index = 0;
+
+        while (index < word.size()) {
+            char c = word[index];
+            /**
+             * Create a child node if none exists matching the prefix.
+             * Place the prefix into the child node.
+            */
+            if (node->children.count(c) == 0) {
+                node->children[c] = std::make_unique<RadixImpl::RadixNode>(word.substr(index));
+                node->children[c]->isEndOfWord = true;
+                return;
+            }
+
+            RadixImpl::RadixNode* child = node->children[c].get();
+            size_t matchingLength = 0;
+            // Check to see how much of the prefix matches.
+            while (matchingLength < child->word.size() && index + matchingLength < word.size() && word[index + matchingLength] == child->word[matchingLength]) {
+                ++matchingLength;
+            }
+            // If the entire child's prefix matches, move to it.
+            if (matchingLength == child->word.size()) {
+                node = child;
+                index += matchingLength;
+            }
+            /**
+             * Else, split the node, look for how long the two match,
+             * once they don't match, save the matching part as the new prefix for the child,
+             * place the rest of child's prefix into a grandchild node,
+             * and create another grandchild, placing the prefix there.
+             */
+            else {
+                auto nodeSplit = std::make_unique<RadixImpl::RadixNode>(child->word.substr(0, matchingLength));
+                nodeSplit->children[child->word[matchingLength]] = std::move(node->children[c]);
+                nodeSplit->children[child->word[matchingLength]]->word = child->word.substr(matchingLength);
+                nodeSplit->isEndOfWord = false;
+                node->children[c] = std::move(nodeSplit);
+
+                if (index + matchingLength < word.size()) {
+                    node->children[c]->children[word[index + matchingLength]] = std::make_unique<RadixImpl::RadixNode>(word.substr(index + matchingLength));
+                    node->children[c]->children[word[index + matchingLength]]->isEndOfWord = true;
+                }
+                else {
+                    node->children[c]->isEndOfWord = true;
+                }
+                return;
+            }
+        }
+        
+        // If the word already exists, throw an exception.
+        if (node->isEndOfWord == true) {
+            throw MyException("The word already exists in the tree");
+        }
+        // Else mark the current node as the end of the word.
+        else {
+            node->isEndOfWord = true;
+        }
+    }
+
+    bool RadixTree::search(const ValueType& word) const{
+        const RadixImpl::RadixNode* node = pImpl->root.get();
+        size_t index = 0;
+
+        while (index < word.size()) {
+            char c = word[index];
+
+            // If the current node doesn't have matching children, return false.
+            if (node->children.count(c) == 0)
+            {
+                return false;
+            }
+
+            const RadixImpl::RadixNode* child = node->children.at(c).get();
+            size_t matchingLength = 0;
+
+            // Check for how long the prefixes match.
+            while (matchingLength < child->word.size() && index + matchingLength < word.size() && word[index + matchingLength] == child->word[matchingLength]) {
+                ++matchingLength;
+            }
+            // If the length doesn't match (meaning the word is shorter than the child prefix), return false.
+            if (matchingLength != child->word.size()) {
+                return false;
+            }
+
+            node = child;
+            index += matchingLength;
+        }
+
+        return node->isEndOfWord;
+    }
+
+    void RadixTree::remove(const ValueType& word) {
+        RadixImpl::RadixNode* node = pImpl->root.get();
+        std::vector<std::pair<RadixImpl::RadixNode*, char>> removablePart;
+        size_t index = 0;
+
+        while (node && index < word.size()) {
+            char c = word[index];
+            // If the child with matching prefix doesn't exist, throw exception.
+            auto existingChild = node->children.find(c);
+            if (existingChild == node->children.end()) {
+                throw MyException("Word not found. Couldn't remove");
+            }
+
+            RadixImpl::RadixNode* child = existingChild->second.get();
+            size_t matchingLength = 0;
+
+            // Check for how long the prefixes match.
+            while (matchingLength < child->word.size() && index + matchingLength < word.size() && word[index + matchingLength] == child->word[matchingLength]) {
+                ++matchingLength;
+            }
+            // If the length doesn't match (meaning the word is shorter than the child prefix), throw exception.
+            if (matchingLength != child->word.size()) {
+                throw MyException("Word not found. Couldn't remove");
+            }
+
+            // Save how much of the tree will be deleted.
+            removablePart.emplace_back(node, c);
+            node = child;
+            index += matchingLength;
+        }
+
+        /**
+         * If:
+         * 1) node is null,
+         * 2) current node's prefix isn't marked as an end of a word OR,
+         * 3) index doesn't match the length of the word,
+         * throw exception.
+         */
+        if (!node || !node->isEndOfWord || index != word.size()) {
+            throw MyException("Word not found. Couldn't remove");
+        }
+
+        // Remove this prefix as the end of the word.
+        node->isEndOfWord = false;
+
+        /**
+         * Iterate backwards through the nodes (leaf to root),
+         * checking to see if the child isn't marked as a word ending,
+         * and doesn't have children.
+         * If not - deletes it, else - breaks the cycle.
+         */
+        for (int i = removablePart.size() - 1; i >= 0; --i) {
+            RadixImpl::RadixNode* parent = removablePart[i].first;
+            char c = removablePart[i].second;
+            RadixImpl::RadixNode* child = parent->children[c].get();
+
+            if (!child->isEndOfWord && child->children.empty()) {
+                parent->children.erase(c);
+            }
+            else {
+                break;
+            }
+        }
+    }
+
+    std::string RadixTree::toString() const {
+        std::vector<ValueType> allSavedWords;
+        pImpl->collectAllWords(pImpl->root.get(), "", allSavedWords);
+        std::ostringstream os;
+
+        for (const ValueType& currentWord : allSavedWords) {
+            os << currentWord << ", ";
+
+        }
+
+        return os.str();
+    }
 
     bool RadixTree::operator==(const RadixTree& other) const{
         return pImpl->compareTrees(pImpl->root.get(), other.pImpl->root.get());
@@ -336,7 +334,7 @@ namespace RadixTreeProject {
     }
 
     bool RadixTree::operator>=(const RadixTree& other) const {
-        return !(*this <= other);
+        return ((*this > other) || (*this == other));
     }
 
     RadixTree& RadixTree::operator+=(const RadixTree& other) {
@@ -368,6 +366,6 @@ namespace RadixTreeProject {
     }
 
     bool RadixTree::operator[](const ValueType& word) const {
-        return pImpl->search(word);
+        return search(word);
     }
 }
