fb0db55a7b27fe83e5f122746f6dcd19e5d5b971
Rave1s <arturas.timofejevas05@gmail.com>
2025-09-15 17:52:48 +0300

    Prideti komentarai

--- RadixTree.cpp
+++ RadixTree.cpp
@@ -7,21 +7,37 @@
 namespace RadixTreeProject {
     class RadixTree::RadixImpl{
         private:
+            /**
+             * @brief Represents a radix tree's node.
+             */
             struct RadixNode {
-                ValueType word;
-                std::unordered_map<char, std::unique_ptr<RadixNode>> children;
-                bool isEndOfWord;
-                RadixNode(const ValueType& wrd = "") : word(word), isEndOfWord(false) {
+                ValueType word; // The prefix being stored in the node.
+                std::unordered_map<char, std::unique_ptr<RadixNode>> children; // Map of all children of the current node.
+                bool isEndOfWord; // Marker to mark wheter this node represents an ending of a word.
+
+                /**
+                 * @brief Constructs an empty radix tree node.
+                 * @param wrd The word (prefix) to be saved at the current node.
+                 * By default isEndOfWord marker is assigned to false,
+                 * as the program will determine whether this is trule the end of a word later.
+                 */
+                RadixNode(const ValueType& wrd = "") : word(wrd), isEndOfWord(false) {
 
                 };
             };
 
         public:
+            /**
+             * @brief Constructor that automatically craetes a root node.
+             */
             std::unique_ptr<RadixNode> root;
             RadixImpl() : root(std::make_unique<RadixNode>()) {
 
             }
 
+            /**
+             * @brief Destructor that frees any alocated memory.
+             */
             ~RadixImpl() {
                 deleteTree();
             }
@@ -32,6 +48,10 @@ namespace RadixTreeProject {
 
                 while (index < word.size()) {
                     char c = word[index];
+                    /**
+                     * Create a child node if none exists matching the prefix.
+                     * Place the prefix into the child node.
+                    */
                     if (node->children.count(c) == 0) {
                         node->children[c] = std::make_unique<RadixNode>(word.substr(index));
                         node->children[c]->isEndOfWord = true;
@@ -40,13 +60,21 @@ namespace RadixTreeProject {
 
                     RadixNode* child = node->children[c].get();
                     size_t matchingLength = 0;
+                    // Check to see how much of the prefix matches.
                     while (matchingLength < child->word.size() && index + matchingLength < word.size() && word[index + matchingLength] == child->word[matchingLength]) {
                         ++matchingLength;
                     }
+                    // If the entire child's prefix matches, move to it.
                     if (matchingLength == child->word.size()) {
                         node = child;
                         index += matchingLength;
                     }
+                    /**
+                     * Else, split the node, look for how long the two match,
+                     * once they don't match, save the matching part as the new prefix for the child,
+                     * place the rest of child's prefix into a grandchild node,
+                     * and create another grandchild, placing the prefix there.
+                     */
                     else {
                         auto nodeSplit = std::make_unique<RadixNode>(child->word.substr(0, matchingLength));
                         nodeSplit->children[child->word[matchingLength]] = std::move(node->children[c]);
@@ -65,9 +93,11 @@ namespace RadixTreeProject {
                     }
                 }
                 
+                // If the word already exists, throw an exception.
                 if (node->isEndOfWord == true) {
                     throw MyException("The word already exists in the tree");
                 }
+                // Else mark the current node as the end of the word.
                 else {
                     node->isEndOfWord = true;
                 }
@@ -80,6 +110,7 @@ namespace RadixTreeProject {
                 while (index < word.size()) {
                     char c = word[index];
 
+                    // If the current node doesn't have matching children, return false.
                     if (node->children.count(c) == 0)
                     {
                         return false;
@@ -88,9 +119,11 @@ namespace RadixTreeProject {
                     const RadixNode* child = node->children.at(c).get();
                     size_t matchingLength = 0;
 
+                    // Check for how long the prefixes match.
                     while (matchingLength < child->word.size() && index + matchingLength < word.size() && word[index + matchingLength] == child->word[matchingLength]) {
                         ++matchingLength;
                     }
+                    // If the length doesn't match (meaning the word is shorter than the child prefix), return false.
                     if (matchingLength != child->word.size()) {
                         return false;
                     }
@@ -109,6 +142,7 @@ namespace RadixTreeProject {
 
                 while (node && index < word.size()) {
                     char c = word[index];
+                    // If the child with matching prefix doesn't exist, throw exception.
                     auto existingChild = node->children.find(c);
                     if (existingChild == node->children.end()) {
                         throw MyException("Word not found. Couldn't remove");
@@ -117,24 +151,41 @@ namespace RadixTreeProject {
                     RadixNode* child = existingChild->second.get();
                     size_t matchingLength = 0;
 
+                    // Check for how long the prefixes match.
                     while (matchingLength < child->word.size() && index + matchingLength < word.size() && word[index + matchingLength] == child->word[matchingLength]) {
                         ++matchingLength;
                     }
+                    // If the length doesn't match (meaning the word is shorter than the child prefix), throw exception.
                     if (matchingLength != child->word.size()) {
                         throw MyException("Word not found. Couldn't remove");
                     }
 
+                    // Save how much of the tree will be deleted.
                     removablePart.emplace_back(node, c);
                     node = child;
                     index += matchingLength;
                 }
 
+                /**
+                 * If:
+                 * 1) node is null,
+                 * 2) current node's prefix isn't marked as an end of a word OR,
+                 * 3) index doesn't match the length of the word,
+                 * throw exception.
+                 */
                 if (!node || !node->isEndOfWord || index != word.size()) {
                     throw MyException("Word not found. Couldn't remove");
                 }
 
+                // Remove this prefix as the end of the word.
                 node->isEndOfWord = false;
 
+                /**
+                 * Iterate backwards through the nodes (leaf to root),
+                 * checking to see if the child isn't marked as a word ending,
+                 * and doesn't have children.
+                 * If not - deletes it, else - breaks the cycle.
+                 */
                 for (int i = removablePart.size() - 1; i >= 0; --i) {
                     RadixNode* parent = removablePart[i].first;
                     char c = removablePart[i].second;
@@ -149,7 +200,9 @@ namespace RadixTreeProject {
                 }
             }
 
-            //TODO add the toString method and the operators
+            /**
+             * @brief Collects all words saved in the radix tree.
+             */
             void collectAllWords(const RadixNode* currentNode, ValueType prefix, std::vector<ValueType>& allWords) const {
                 if (!currentNode) {
                     return;
@@ -179,6 +232,12 @@ namespace RadixTreeProject {
                 return os.str();
             }
 
+            /**
+             * @brief Compares if trees are equal (have the same saved words).
+             * @param thisNode Current node.
+             * @param otherNode Node to compare with.
+             * @return True if they are equal, false otherwise.
+             */
             bool compareTrees(const RadixNode* thisNode, const RadixNode* otherNode) const {
                 if (!thisNode && !otherNode) {
                     return true;
@@ -217,21 +276,26 @@ namespace RadixTreeProject {
                 return copy;
             }
 
+            // Clears the radix tree.
             void emptyTree() {
                 root = std::make_unique<RadixNode>();
             }
 
+            // Deletes the radix tree.
             void deleteTree() {
                 root.reset();
             }
     };
 
+    // Constructor.
     RadixTree::RadixTree() : pImpl(std::make_unique<RadixImpl>()) {
                 
     }
 
+    // Destructor.
     RadixTree::~RadixTree() = default;
 
+    // Deep copy constructor.
     RadixTree::RadixTree(const RadixTree& other) : pImpl(std::make_unique<RadixImpl>()) {
         pImpl->root = RadixImpl::copyRadixTree(other.pImpl->root.get());
     }
--- RadixTree.h
+++ RadixTree.h
@@ -1,3 +1,7 @@
+/**
+ * @author: Arturas Timofejevas (@Rave1s), VU SE 2 course 2 group
+*/
+
 #ifndef RADIX_H
 #define RADIX_H
 
@@ -6,46 +10,145 @@
 #include <memory>
 #include <exception>
 
-//TODO add explanatory comments
 namespace RadixTreeProject {
+
+    /** 
+     * @class RadixTree
+     * @brief Radix tree data structure for storing words.
+     * 
+     * Implementation of the class is hidden using the PImpl idiom.
+    */
+
     class RadixTree {
         private:
+            // Declaration of the class implementation, providing a pointer to the tree.
             class RadixImpl;
             std::unique_ptr<RadixImpl> pImpl;
 
         public:
+            //Creating an alias to be used by the tree's methods.
             using ValueType = std::string;
 
+            //Radix tree constructor - creates an empty tree.
             RadixTree();
 
+            //Destructor - deletes the tree and frees the allocated memory.
             ~RadixTree();
 
+            /**
+             * @brief Deep copy constructor.
+             * @param other The tree you wish to copy.
+             */
             RadixTree(const RadixTree& other);
 
+            /**
+             * @brief Deep copy assignment operator.
+             * @param other The tree you wish to copy from.
+             * @return Reference to the current radix tree.
+             */
             RadixTree& operator=(const RadixTree& other);
 
+            /**
+             * @brief Inserts a new word into the tree.
+             * @param word The word to insert into the tree.
+             * @throws an exception if the word already exists.
+             */
             void insert(const ValueType& word);
 
+            /**
+             * @brief Searches for a word in the tree.
+             * @param word The word to search in the tree.
+             * @return True if the word exists in the tree, false otherwise.
+             */
             bool search(const ValueType& word);
 
+            /**
+             * @brief Removes a given word from the tree.
+             * @param word The word to remove from the tree.
+             * @throws an exception if the word doesn't exist in the tree.
+             */
             void remove(const ValueType& word);
 
+            /**
+             * @brief Returns a string representing each word in the tree.
+             * @return A string containing all words in the tree.
+             */
             std::string toString() const;
 
+            /**
+             * @brief Compares two radix trees for equality.
+             * @param other The radix tree to compare with.
+             * @return True if the trees are equal (have the same words), false otherwise.
+             */
             bool operator==(const RadixTree& other) const;
+
+            /**
+             * @brief Compares to radix trees for inequality.
+             * @param other The radix tree to compare with.
+             * @return True if trees are not equal, false otherwise.
+             */
             bool operator!=(const RadixTree& other) const;
+
+            /**
+             * @brief Checks if current tree has less words saved than another tree.
+             * @param other The radix tree to compare with.
+             * @return True if the current tree has less words than the other tree, false otherwise.
+             */
             bool operator<(const RadixTree& other) const;
+
+            /**
+             * @brief Checks if current tree has more words saved than another tree.
+             * @param other The radix tree to compare with.
+             * @return True if the current tree has more words than the other tree, false otherwise.
+             */
             bool operator>(const RadixTree& other) const;
+
+            /**
+             * @brief Checks if the current radix tree is equal to the other or has less words.
+             * @param other The radix tree to compare with.
+             * @return True if either condition if met, false otherwise.
+             */
             bool operator<=(const RadixTree& other) const;
+
+            /**
+             * @brief Checks if the current radix tree is equal to the other or has more words.
+             * @param other The radix tree to compare with.
+             * @return True if either condition if met, false otherwise.
+             */
             bool operator>=(const RadixTree& other) const;
 
+            /**
+             * @brief Merges the current tree with another.
+             * @param other The tree to merge into the current one.
+             * @return Reference to the current radix tree.
+             */
             RadixTree& operator+=(const RadixTree& other);
+
+            /**
+             * @brief Removes another tree's words from the current one.
+             * @param other The tree words of which will be removed from the current one.
+             * @return Reference to the current radix tree.
+             */
             RadixTree& operator-=(const RadixTree& other);
+
+            /**
+             * @brief Clears the current radix tree but doesn't delete it.
+             * @return Reference to the current radix tree.
+             */
             RadixTree& operator!();
+
+            /**
+             * @brief Searches for the given word in the tree.
+             * @param word The word to be searched.
+             * @return True if the word exists in the tree, false otherwise.
+             */
             bool operator[](const ValueType& word) const;
 
     };
 
+    /**
+     * @brief Exception class to handle exceptions appearing with tree's methods.
+     */
     class MyException : public std::exception {
         private:
             std::string message;
